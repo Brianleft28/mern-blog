3bf3a86c7c7aa9bf62f7bccbc3d045b3273ce89e
Brian <41770471+Brianleft28@users.noreply.github.com>
2024-02-12 15:09:20 -0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+    // conexión a la base de datos
+mongoose
+    .connect(
+       process.env.MONGO
+    )
+    .then( () => {
+        console.log('The database is connected succesfully'
+    )})
+    
+    // si logro autenticar en mongoDb, que se imprima este mensaje con un metodo .then)
+
 
 const app = express();
 
